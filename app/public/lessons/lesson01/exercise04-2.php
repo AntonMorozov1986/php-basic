8271c4fea3d7b4aeddfe2ddd1a3c27429e2e5bfd
anton morozov <amorozov86@gmail.com>
2022-02-26 20:00:00 +0300

    Completed the fourth exercise

---
+++ app/public/lessons/lesson01/exercise04-2.php
@@ -0,0 +1,8 @@
+<?php
+$title = "Главная страница - страница обо мне";
+$heading = "Информация обо мне";
+$currentYear = date ('Y');
+$imgSrc = "images/bee.jpg";
+
+include "exercise04-2-template.php";
+

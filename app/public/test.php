cd5f5f5102b055b3959a4df03cd3e51ca22ec13c
anton morozov <amorozov86@gmail.com>
2022-03-19 22:19:07 +0300

    update

--- app/public/test.php
+++ app/public/test.php
@@ -11,7 +11,7 @@ echo 'MySQL version:' . $row['Value'];
 ?>
 <h3>Проверка работы php</h3>
 <p>Если вы видите версию mySQL, то это значит, что все запущено успешно. &#128640; &#127881;</p>
-<a class="link" href="index.html">&#9166; Вернуться на главную</a>
+<a class="link" href="index.php">&#9166; Вернуться на главную</a>
 <style>
 .link {
     text-decoration: none;
